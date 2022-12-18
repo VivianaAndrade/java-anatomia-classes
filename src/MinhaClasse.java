f7c1ca8a967c3180f85ffb0b728b6fea90a454c9
VivianaAndrade <vivibecker.andrade@gmail.com>
2022-12-18 13:27:03 -0300

    first commit

---
+++ src/MinhaClasse.java
@@ -0,0 +1,22 @@
+public class MinhaClasse {
+public static void main (String [] args){
+
+    int ano = 2021; //pode sofrer alterações
+    ano = 2022;
+
+    final String BR = "Brasil";//a palavra "final" não permite alterações.
+
+    String primeiroNome = "Viviana";
+    String segundoNome = "Andrade";
+    
+    String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+    System.out.println(nomeCompleto);
+}
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do Metodo:"+segundoNome.concat(" ").concat(segundoNome);
+   
+}
+
+}
+
